bb988a32ee3797b1cdb4b7083810b8dac8adfcc7
dan-pdk <danielfteste20222@gmail.com>
2025-04-12 21:05:39 -0300

    Tried to add dark mode (without success)

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// alterna a classe .darkable para cada elemento com a classe "darkable"
+function toggleDarkMode() {
+    const elements = document.body
+    elements.classList.toggle(".dark-mode")
+    console.log("i'm working");
+    console.log(elements)
+}
