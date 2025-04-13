34285ac0cc4fa8b37a7e5a48a7ed22f0291e0c58
Arthur-IFPR <arthurvalentinscoparoifpr@gmail.com>
2025-04-13 13:31:50 -0300

    made code loke 0.1% prettier

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleDarkMode() {
     const elements = document.querySelectorAll(".darkable")
     elements.forEach(element => {
-
         const currentClass = element.className
         element.className = `${currentClass} dark-mode`
     });
