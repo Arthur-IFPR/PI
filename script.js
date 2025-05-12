5bc602f5138775e4f38bef144f3c89f0be7a8be8
dan-pdk <danielfllores2020@gmail.com>
2025-05-12 11:13:49 -0300

    refeito o método de criação do modal

--- script.js
+++ script.js
@@ -12,69 +12,18 @@ function toggleDarkMode() {
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(element => {
-    element.addEventListener('click', () => {
-        const modalBackground = document.createElement('div')
-        modalBackground.className = 'modal-background'
-        document.body.appendChild(modalBackground)
-
-        const modal = document.createElement("div");
-        modal.className = 'modal darkable'
-        //TODO - fix
-        if (darkmode) {
-            modal.style.backgroundColor = '#292929'
-            modal.style.color = 'white'
-        }
-        if (!darkmode) {modal.style.backgroundColor = 'white'}
-        
-        modalBackground.appendChild(modal)
-        const content = createModalContent();
-        console.log(content, 'created content')
-        appendContent(content, modal)
-        
-        document.body.style.overflowY = 'hidden'
+    element.addEventListener('click', event => {
+        const cardTopic = event.target.id
+        toggleModal(cardTopic)
     })
 })
 
-function createModalContent() {
-    const content = [
-        {
-          type: 'div',
-          content: "",
-          class: "closeButtonContainer",
-          children: [
-            {
-                type: 'div',
-                content: '<p>x</p>',
-                class: "closeButton",
-                events: [
-                    {
-                        type: 'click',
-                        handler: () => {
-                            const modalBackground = document.querySelector('.modalBackground');
-                            document.body.removeChild(modalBackground)
-                        }
-                    }
-                ]
-            }
-          ]
-        }
-    ]
-    return content
+
+function toggleModal(cardTopic) {
+    console.log(cardTopic)
+    const modal = document.querySelector(`#card-${cardTopic}`);
+    console.log(modal)
+    modal.classList.toggle('hide-modal');
 }
 
-function appendContent(content, parent) {
-    content.forEach(elementData => {
-        const element = document.createElement(elementData.type);
-        if (elementData.content !== '') {
-            element.innerHTML = elementData.content
-        }
-        element.className = elementData.class;
-        if (elementData.events) {
-            elementData.events.forEach(event => {
-                element.addEventListener(event.type, event.handler)
-            })
-        }
-        parent.appendChild(element);
-        elementData.children && appendContent(elementData.children, element)
-    })
-}
+
